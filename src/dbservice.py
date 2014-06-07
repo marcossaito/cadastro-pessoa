973a47d220067d7e1a5bd07d4371e5655485f844
Marcos Saito <m.saito@consultoriasigma.com.br>
2014-06-07 10:04:02 -0300

    Commit correto

--- src/dbservice.py
+++ src/dbservice.py
@@ -5,18 +5,36 @@ class DBservice(object):
 
 	def __init__(self,string):
 
-		if os.path.isfile(string + ".db"):
+		if not (os.path.isfile(string + ".db")):
 			self.conn = sqlite3.connect(string + ".db")
+			self.c = self.conn.cursor()
+			self.c.execute(''' CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, cpf text, data_nascimento date)''')
+	
+			self.conn.commit()
 			self.dbname = string
+		
+		elif os.path.isfile(string + ".db"):
+			self.conn = sqlite3.connect(string + ".db")
+			self.c = self.conn.cursor()
+			self.dbname = string
+
+		elif os.path.isfile("appdb.db"):
+			self.conn = sqlite3.connect(string + ".db")
+			self.c = self.conn.cursor()
+			self.dbname = "appdb"
+
 		else:
 			self.conn = sqlite3.connect("appdb.db")
-			self.c = conn.cursor()
+			self.c = self.conn.cursor()
+			self.c.execute(''' CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, cpf text, data_nascimento date)''')
+	
+			conn.commit()
 			self.dbname = "appdb"
 
 	def create_tables(self):
 		
-		if os.path.isfile(self.dbname+".db")
-			self.c.execute(''' CREATE TABLE user (id INTEGER AUTOINCREMENT, name text, cpf text, data_nascimento date)''')
+		if os.path.isfile(self.dbname+".db"):
+			self.c.execute(''' CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, cpf text, data_nascimento date)''')
 			self.conn.commit()
 
 		else:
@@ -24,13 +42,28 @@ class DBservice(object):
 
 		return True
 
-	def insert_user(self,nome,cpf,data_nascimento):
+	def insert_user(self,insert_tuple):
 		
 		if os.path.isfile(self.dbname+".db"):
-			self.c.execute(''' INSERT INTO user VALUES (NULL,?,?,?)''',(nome,cpf,data_nascimento))
+			self.c.execute('''INSERT INTO user VALUES (NULL,?,?,?)''',insert_tuple)
 			self.conn.commit()
 
 		else:
 			return False
 
 		return True
+
+def create_default_db():
+	conn = sqlite3.connect("appdb.db")
+	c = conn.cursor()
+
+	c.execute(''' CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT, name text, cpf text, data_nascimento date)''')
+	
+	conn.commit()
+	conn.close()
+
+	print "All Done!"
+
+if __name__ == "__main__":
+	create_default_db()
+
--- src/mensseges.py
+++ src/mensseges.py
@@ -1,12 +1,12 @@
-import kivy.uix.popup import Popup
-import kivy.uix.button import Button
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
 
 class MenssegerPopup(object):
 
 	def __init__(self,title,content):
 		self.btn = Button(text=content)
-		self.popup = Popup(title=title,content=self.btn,auto_dismiss=False)
-		self.btn.bind(on_press=self.popup.dismiss())
+		self.popup = Popup(title=title,content=self.btn,auto_dismiss=False,size_hint=(.5,.2),pos=(.2,.2))
+		self.btn.bind(on_press=self.popup.dismiss)
 
 	def show_me(self):
 		self.popup.open()
