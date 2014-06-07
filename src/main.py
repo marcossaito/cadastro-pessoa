70ef6e5ee7eca558ea2107994f1b52881c541dc4
Marcos Saito <m.saito@consultoriasigma.com.br>
2014-06-07 10:00:45 -0300

    Integracao com o banco de dados SQLITE3 e Ajuste de Menssagens

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -5,6 +7,9 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+
+from mensseges import MenssegerPopup
+from dbservice import DBservice
 
 class MyApp(App):
 
@@ -18,20 +23,52 @@ class MyApp(App):
 
 	def create_buttons(self):
 		self.btn_register = Button(text = "Cadastrar")
+		self.btn_register.bind(on_press=self.action_btn_register)
+
 		self.btn_apagar = Button(text = "Apagar")
+		self.btn_apagar.bind(on_press=self.clear_all_text)
 
 		self.layout.add_widget(self.btn_register)
 		self.layout.add_widget(self.btn_apagar)
 
+	def action_btn_register(self,instance):
+		db = DBservice("sigma")
+		input_texts = []
+
+		for index in range(len(self.texts_on_grid)):
+			input_texts.append(self.texts_on_grid[index].text)
+
+		t = tuple(input_texts)
+
+		confirm = db.insert_user(t)
+
+		if confirm:
+			popup = MenssegerPopup("Sucsses","All Done!")
+			popup.show_me()
+
+		else:
+			MenssegerPopup("Error","Ok")
+			popup.show_me()
+			self.clear_all_text(self.btn_register)
+
+		self.clear_all_text(instance)
+
+	def clear_all_text(self,instance):
+
+		for text_input  in self.texts_on_grid:
+			text_input.text = ''
+
 	def create_labels_text(self):
-		names = ['Nome', 'CPF', 'Data de Nascimento','CoolSttuf']
-		self.lines_on_grid = []
+		names = ['Nome', 'CPF', 'Data de Nascimento']
+		self.texts_on_grid = []
+		self.labels_on_grid = []
 
 		for name in names:
 			text = TextInput(multiline=False)
+			self.texts_on_grid.append(text)
 			label = Label(text=name)
+			self.labels_on_grid.append(label)
 
-			line = (label,text)
 			self.layout.add_widget(label)
 			self.layout.add_widget(text)
 
